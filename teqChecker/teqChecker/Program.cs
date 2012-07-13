a948eb89744bf6cdd085715e55621fb53a2f6937
Todd Qualls (Enoch) <tqualls@gmail.com>
2012-07-12 22:57:25 -0400

    Adds teqChecker console app for copying files to teqBlog/images
    Copies files from iOS DropBox "Camera Uploads" folder.

---
+++ teqChecker/teqChecker/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Configuration;
+using System.Net.Configuration;
+using System.IO;
+using System.Web;
+
+namespace teqChecker
+{
+    class Program
+    {
+        private static string monitorDir = ConfigurationManager.AppSettings["DirectoryToMonitor"];
+        private static string copyToDir = ConfigurationManager.AppSettings["CopyToDirectory"];
+
+        static void Main(string[] args)
+        {
+            //Check monitored folder and move all files to images folder
+            string[] files = Directory.GetFiles(monitorDir);
+            foreach (string f in files)
+            {
+                string fileName = Path.GetFileName(f);
+                fileName += fileName.Replace(" ", "_");
+                string destFileName = Path.Combine(copyToDir, fileName);
+                File.Copy(f, destFileName,false);
+            }
+
+            //teqBlogEntities context = new teqBlogEntities();
+            //{
+
+            //}
+        }
+    }
+}
